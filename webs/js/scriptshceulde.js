7ad0c3916c3105b9148708ac513fec5a6480f401
Teo <extremteogamer17yt@gmail.com>
2024-03-07 17:17:42 +0100

    Commit Teo JS update

--- webs/js/scriptshceulde.js
+++ webs/js/scriptshceulde.js
@@ -1,3 +1,4 @@
+/*
 const elements = document.querySelectorAll(".row--details");
 function toggleRow(row) {
     for(let i = 0; i < elements.length; i++){
@@ -14,6 +15,25 @@ function toggleRow(row) {
             elements[i].classList.remove("unshow");
     }
 
-    const details = row.nextElementSibling;
     details.classList.toggle("show");
   }
+  */
+  function toggleRow(row) {
+    const elements = document.querySelectorAll(".row--details");
+
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].classList.contains("show") && elements[i] !== row.nextElementSibling) {
+            elements[i].classList.add("unshow");
+            setTimeout(() => {
+                elements[i].classList.remove("show", "unshow");
+            }, 600);
+        }
+    }
+
+    const details = row.nextElementSibling;
+    if (details && details.classList.contains("row--details")) {
+        details.classList.toggle("show");
+        row.removeAttribute("onclick");
+    }
+}
+
